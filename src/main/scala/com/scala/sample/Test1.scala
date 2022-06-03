7623be55a40a738afe19c603a9bf029d9510efbd
Knaveen333 <naveen.kovuru@gmail.com>
2022-06-03 12:52:18 +0530

    First commit

---
+++ src/main/scala/com/scala/sample/Test1.scala
@@ -0,0 +1,16 @@
+package com.scala.sample
+
+object Test1 {
+
+  def main(args: Array[String]): Unit = {
+
+      val hungry = "yes"
+
+      if (hungry == "yes")
+          println("eat something...")
+      else
+          println("Do work...")
+
+  }
+
+}
